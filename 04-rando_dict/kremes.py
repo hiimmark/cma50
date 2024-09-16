265393d4b27ea0c110e252b35d8849f95c09ef02
Mark Ma <markworldamc@gmail.com>
2024-09-16 10:32:41 -0400

    wk #4

---
+++ 04-rando_dict/kremes.py
@@ -0,0 +1,28 @@
+# Mark Ma
+# Ghidorah 
+# SoftDev
+# Python dictionary and random selection. Select a random person from the dictionary.
+# 2024-09-13
+# time spent: 0.3
+
+from random import randint
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+rand_period = randint(4, 5)
+rand_index = randint(0, len(krewes[rand_period]) - 1)
+rand_person = krewes[rand_period][rand_index]
+print(rand_person)
