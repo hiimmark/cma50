3040536d92808ca8bdcffb45b1a880d8305e41be
Mark Ma <markworldamc@gmail.com>
2024-09-27 10:48:37 -0400

    app upd

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -21,15 +21,18 @@ The third argument passes the coll array into the collections variable in the te
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Observed:
 When render_template is removed from input, localhost:5000/my_foist_template displays a NameError and a long
-erorr message is printed in terminal.
+erorr message is printed in terminal. But, the server is still hosted and the root page loads fine.
 
 When localhost:5000/my_foist_template is loaded, the title is foooo, and 0 1 1 2 3 5 8 is displayed on
 different lines. It seems to us that our prediction in Q3 is correct. "foooo" is assigned the foo variable
 in the html template file and passed through the render_template function in app.py, and the collections variable
 is assigned the coll array in app.py.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+QCC:
+1. Can all variables be passed through render_template?
+2. Does flask read through the template file and extract all variables from the Jinja code to ensure your arguments are valid?
+3. Why can't we initialize these variables in the Jinja code itself? Why do we have to pass these as parameters?
 """
-
-
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q0: What will happen if you remove render_template from the following statement?
