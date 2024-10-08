1709c183e2a05437c31a493d8b5de7041f510220
Mark Ma <markworldamc@gmail.com>
2024-10-08 10:50:02 -0400

    15

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -1,17 +1,21 @@
+'''
+Mark Ma
+Ghidorah - Mark, Danny, Amanda
+SoftDev
+K15 - Flask Forms
+2024-10-07
+time spent: 0.5
+'''
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
 
-
-
 app = Flask(__name__)    #create Flask object
 
-
-
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/")
 def disp_loginpage():
-    return render_template( 'login.html' )
+    return render_template( 'login.html' , method = request.method)
 
 
 @app.route("/response", methods=['POST']) # , methods=['GET', 'POST'])
