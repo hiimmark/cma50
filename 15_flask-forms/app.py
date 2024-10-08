5fbd5b57f4d6c4ae8c8bd0c8977e198bcac7a36a
Mark Ma <markworldamc@gmail.com>
2024-10-08 10:47:10 -0400

    15

---
+++ 15_flask-forms/app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+
+
+app = Flask(__name__)    #create Flask object
+
+
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+
+@app.route("/response", methods=['POST']) # , methods=['GET', 'POST'])
+def authenticate():
+    return render_template('response.html', username=request.form['username'], method=request.method)
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
