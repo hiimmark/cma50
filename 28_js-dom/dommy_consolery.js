823d6db2721783990782a794640a1e86d23598fd
Mark Ma <markworldamc@gmail.com>
2025-01-07 11:14:57 -0500

    a

---
+++ 28_js-dom/dommy_consolery.js
@@ -0,0 +1,106 @@
+//    Team TopherAcademy1 :: Mark Ma, Anastasia Lee
+//    SoftDev pd4
+//    K28 -- Manipulating the DOM
+//    2025-01-07t
+
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) 
+{
+    var j=30;
+    return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 1024,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+              return x+30;
+          }
+        };
+
+//create a new node in the tree
+var addItem = function(text)
+{
+    var list = document.getElementById("thelist");
+    var newitem = document.createElement("li");
+    newitem.innerHTML = text;
+    list.appendChild(newitem);
+};
+
+//prune a node from the tree
+var removeItem = function(n)
+{
+    var listitems = document.getElementsByTagName('li');
+    listitems[n].remove();
+};
+
+//color selected elements red
+var red = function()
+{
+    var items = document.getElementsByTagName("li");
+    for(var i = 0; i < items.length; i++) {
+	items[i].classList.add('red');
+    }
+};
+
+//color a collection in alternating colors
+var stripe = function()
+{
+    var items = document.getElementsByTagName("li");
+    for(var i = 0; i < items.length; i++) {
+	if (i%2==0) {
+	    items[i].classList.add('red');
+	} else {
+	    items[i].classList.add('blue');
+	}
+    }
+};
+
+
+//insert your implementations here for...
+// FIB
+// FAC
+// GCD
+
+const fib = (n) => {
+	if(n <= 1) return n;
+	return fib(n-1) + fib(n-2);
+};
+
+const fac = (n) => {
+	if(n == 1) return 1;
+	return n * fac(n-1);
+};
+
+const gcd = (a, b) => {
+	if(b == 0) return a;
+	return gcd(b, a % b);
+};
+
+console.log(fib(12));
+console.log(fac(9));
+console.log(gcd(33,69));
+
+// In addition to the style shown above,
+//  you are encouraged to test drive the "arrow function syntax" as shown below.
+//  Note anything notable.
+const myFxn = (param1, param2) => {
+    // body
+    return retVal;
+};
+
+
